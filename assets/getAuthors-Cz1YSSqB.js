ea79b415b145a1b692850329052261a5c9051fad
Denis <deni4gt@gmail.com>
2024-07-07 14:57:31 +0200

    Updates

---
+++ assets/getAuthors-Cz1YSSqB.js
@@ -0,0 +1,1 @@
+const n=e=>{const s=[];for(let t=0;t<e.length;t++)s.push(e[t].name);return s.join(", ")};export{n as g};
---
+++ assets/getTimeFromMs-CvwRTgYM.js
@@ -0,0 +1,1 @@
+function s(n){const e=new Date(n),t=e.getSeconds();return t<10?`${e.getMinutes()}:0${t}`:`${e.getMinutes()}:${e.getSeconds()}`}export{s as g};
